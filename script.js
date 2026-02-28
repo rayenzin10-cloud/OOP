1989b05801d46f435b9d75ba9c469a40f9cc6f72
rayenzin10-cloud <rayenzin10@gmail.com>
2026-02-28 13:31:37 +0100

    update

---
+++ script.js
@@ -0,0 +1,42 @@
+function voiture(initial, initial1, initial2, initial3) {
+
+    var voiture = {};
+    voiture.nombreDePassager = initial;
+    voiture.nombreMaxDePassagers = initial1;
+    voiture.carburant = initial2;
+    voiture.kilometrage = initial3;
+    voiture.monter = monter;
+    voiture.descendre = descendre;
+    voiture.rouler = rouler;
+
+    return compteur;
+}
+
+var monter = function () {
+    if (this.nombreDePassager < this.nombreMaxDePassagers) {
+        this.nombreDePassager++;
+    } else return "plus de place"
+
+};
+
+var descendre = function () {
+    if (this.nombreDePassager > 0) {
+        this.nombreDePassager--;
+    } else return "personne a bord"
+};
+
+var rouler = function (mile) {
+    this.kilometrage += mile;
+    this.carburant = kilometrage / nombreDePassager;
+    if (this.nombreDePassager === 0) {
+        return "pas de conducteur";
+    }
+    if (this.carburant < this.kilometrage) {
+        return "carburant insuffisant";
+    }
+};
+
+var voiture1 = voiture(7, 8, 50, 100)
+voiture1.monter();
+voiture1.descendre();
+voiture1.rouler(100);
